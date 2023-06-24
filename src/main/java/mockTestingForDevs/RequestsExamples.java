9345903879910b8fea375cf8cef472291bf35beb
Artur <arturmuxutdinov@gmail.com>
2023-06-24 13:59:27 +0300

    fix(requests):
    FIxed issue with the null returning in the BookRoom and other methods. For now, all works correctly; Tested

--- src/main/java/mockTestingForDevs/RequestsExamples.java
+++ src/main/java/mockTestingForDevs/RequestsExamples.java
@@ -1,10 +1,8 @@
 package mockTestingForDevs;
 
-import APIWrapper.json.BookingsFilter;
-import APIWrapper.json.GetFreeRoomsRequest;
-import APIWrapper.json.QueryBookingsRequest;
-import APIWrapper.json.Room;
+import APIWrapper.json.*;
 import APIWrapper.requests.Request;
+import com.google.gson.GsonBuilder;
 
 import java.util.ArrayList;
 
@@ -12,17 +10,17 @@ public class RequestsExamples {
     public static void main(String[] args) {
         Request request = new Request("http://localhost:3000");
 
-        var getFreeRoomsRequest = new GetFreeRoomsRequest(
-                "22.06.23 04:30",
-                90
-        );
-
-//        BookRoomRequest bookRoomRequest = new BookRoomRequest(
-//                "Test bookings from the mockTesting... directory",
-//                "22.06.23 05:00",
-//                90,
-//                "email@innopolis.university"
+//        var getFreeRoomsRequest = new GetFreeRoomsRequest(
+//                "22.06.23 04:30",
+//                90
 //        );
+
+        BookRoomRequest bookRoomRequest = new BookRoomRequest(
+                "Test bookings from the mockTesting... directory",
+                "22.06.23 05:00",
+                90,
+                "email@innopolis.university"
+        );
 
         QueryBookingsRequest queryBookingsRequest = new QueryBookingsRequest(
                 new BookingsFilter(
@@ -34,13 +32,17 @@ public class RequestsExamples {
         );
 
 //        ArrayList<Room> rooms = request.getAllBookableRooms();
-        ArrayList<Room> freeRooms = request.getAllFreeRooms(getFreeRoomsRequest);
-        for (Room room : freeRooms) {
-            System.out.println(room.name);
-        }
+//        ArrayList<Room> freeRooms = request.getAllFreeRooms(getFreeRoomsRequest);
+//        for (Room room : freeRooms) {
+//            System.out.println(room);
+//        }
 //        Booking booking = request.bookRoom("3.5", bookRoomRequest);
+//        System.out.println(new GsonBuilder().setPrettyPrinting().create().toJson(booking));
 //        ArrayList<Booking> bookings = request.queryBookings(queryBookingsRequest);
-//        String deleteReport = request.deleteBooking("2");
-//        ArrayList<Booking> bookings = request.getBookingsByUser("email@innopolis.university");
+//        System.out.println(bookings);
+//        bookings.forEach(booking -> System.out.println(booking.title));
+//        String deleteReport = request.deleteBooking("1");
+        ArrayList<Booking> bookings = request.getBookingsByUser("email@innopolis.university");
+        System.out.println(bookings);
     }
 }
