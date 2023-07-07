efefb9e6c7d7fa8ed9190b8aabe91bb73c7dae14
yeaphm <efimpuzalov@gmail.com>
2023-07-07 22:03:00 +0300

    feature(Tests): Unit test
    refactored unit tests for the pipeline

--- src/test/java/Database/Services/UserDataServiceTest.java
+++ src/test/java/Database/Services/UserDataServiceTest.java
@@ -1,7 +1,7 @@
 package Database.Services;
 
 import Database.DbConnector;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
